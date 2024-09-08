914432453afb2985499232f0694ae1003cff6a8b
Zzzas <juanpmc7722@gmail.com>
2024-09-07 20:32:58 -0500

    Se adiciono el código de división

--- src/main/java/ejercicios/Division.java
+++ src/main/java/ejercicios/Division.java
@@ -10,15 +10,22 @@ import java.util.Scanner;
  */
 public class Division {
     public static String evaluar(int dividendo, int divisor) {
-        // TODO: Coloca aquí el código del ejercicio 3: Division
-        int cociente = 0;
-        int residuo = 0;
-        String respuesta = "La división es exacta. \n"
-                + "Cociente: " + cociente + "\n"
-                + "Residuo: " + residuo;
+        int cociente = dividendo / divisor;
+        int residuo = dividendo % divisor;
+        
+        String respuesta;
+        if (residuo == 0) {
+            respuesta = "La división es exacta. \n";
+        } else {
+            respuesta = "La división no es exacta. \n";
+        }
+        
+        respuesta += "Cociente: " + cociente + "\n";
+        respuesta += "Residuo: " + residuo;
+        
         return respuesta;
     }
-    
+ 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Dividendo:");
