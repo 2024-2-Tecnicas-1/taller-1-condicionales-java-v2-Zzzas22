e69118f0b46929b19a3dbd84cc88060bea845641
Zzzas <juanpmc7722@gmail.com>
2024-09-07 19:49:39 -0500

    Se adiciono el código para determinar si el año es bisiesto o no.

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -11,7 +11,11 @@ import java.util.Scanner;
 public class AnnosBisiestos {
     public static String evaluar(int anno){
         // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-        return "";
+        if((anno % 4 == 0 && anno % 100 != 0) || (anno % 400 == 0)){
+            return anno + " es bisiesto";
+        }else{
+            return anno + " no es bisiesto";
+        }
     }
     
     public static void main(String[] args) {
