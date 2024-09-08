b3c609157d6183da3d5d8558a412b41e3c530281
Zzzas <juanpmc7722@gmail.com>
2024-09-07 21:51:37 -0500

    Se adiciono el codigo para determinar la derrota, victoria o empate en los set de tennis

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -7,9 +7,22 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class SetDeTenis {
-    public static String evaluar(int numVictoriasA, int numVictoriasB) {
-        // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-        return "";
+   public static String evaluar(int numVictoriasA, int numVictoriasB) {
+        if (numVictoriasA < 0 || numVictoriasB < 0 || 
+            numVictoriasA > 7 || numVictoriasB > 7 ||
+            (numVictoriasA == 7 && numVictoriasB < 5) ||
+            (numVictoriasB == 7 && numVictoriasA < 5)) {
+            return "Inválido";
+        }
+
+        if (numVictoriasA >= 6 || numVictoriasB >= 6) {
+            if (Math.abs(numVictoriasA - numVictoriasB) >= 2 || 
+                numVictoriasA == 7 || numVictoriasB == 7) {
+                return (numVictoriasA > numVictoriasB) ? "Ganó A" : "Ganó B";
+            }
+        }
+        
+        return "Aún no termina";
     }
     
     public static void main(String[] args) {
