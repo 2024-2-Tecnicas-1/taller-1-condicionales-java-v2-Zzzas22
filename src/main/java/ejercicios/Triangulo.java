358a4d81e3deaad46ab950b5faf80826cfe43b2e
Zzzas <juanpmc7722@gmail.com>
2024-09-07 21:58:56 -0500

    Se adiciono código para determinar que tipo de triangulo es según el caso

--- src/main/java/ejercicios/Triangulo.java
+++ src/main/java/ejercicios/Triangulo.java
@@ -9,8 +9,19 @@ import java.util.Scanner;
  */
 public class Triangulo {
     public static String evaluar(double a, double b, double c) {
-        // TODO: Coloca aquí el código del ejercicio 7: Triángulos
-        return "";
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return "No es un triángulo válido";
+        }
+        
+        if (a == b && b == c) {
+            return "El triángulo es equilátero";
+        }
+        
+        if (a == b || b == c || a == c) {
+            return "El triángulo es isósceles";
+        }
+        
+        return "El triángulo es escaleno";
     }
     
     public static void main(String[] args) {
